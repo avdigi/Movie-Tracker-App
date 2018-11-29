bccafbdeec4d22332c4fcda0dd0973b361c3aea3
Anthony DiGiovanni <avdigi@saras-macbook-pro-2.local>
2018-11-29 08:47:47 -0500

    Initial Commit

---
+++ MovieApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MovieApp
+//
+//  Created by Anthony DiGiovanni on 11/29/18.
+//  Copyright © 2018 Anthony DiGiovanni. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
